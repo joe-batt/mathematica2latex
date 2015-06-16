49cc219d0f0d0325fee9a8d468413bd14326ad8e
Johannes Battenberg <battenberg@stud.tu-darmstadt.de>
2015-06-16 23:11:59 +0200

    initial commit

---
+++ src/de/tud/mathematica2latex/Regex.java
@@ -0,0 +1,9 @@
+package de.tud.mathematica2latex;
+
+public class Regex {
+
+	public static void main(String[] args) {
+		String s="\\[Alpha]";
+		System.out.println(s.matches(".*\\\\\\[[A-Za-z]+\\].*"));
+	}
+}
