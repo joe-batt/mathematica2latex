f279f2ff2612e015bb97604eabcf0987084202af
Johannes Battenberg <battenberg@stud.tu-darmstadt.de>
2015-06-16 23:41:57 +0200

    removed useless test case

--- src/eu/chaoticneutral/mathematica2latex/Regex.java
+++
@@ -1,9 +0,0 @@
-package eu.chaoticneutral.mathematica2latex;
-
-public class Regex {
-
-	public static void main(String[] args) {
-		String s="\\[Alpha]";
-		System.out.println(s.matches(".*\\\\\\[[A-Za-z]+\\].*"));
-	}
-}
